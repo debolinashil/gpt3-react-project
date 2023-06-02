6a1d1a044b1d89e0e8a0bb9ae3fc078923b56bc8
Debolina Shil <debolina.shil96@gmail.com>
2023-06-02 22:33:23 +0530

    first commit to repository

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Articles } from "./articles/Article";
+export { default as Brand } from "./brand/Brand";
+export { default as CTA } from "./cta/CTA";
+export { default as Feature } from "./feature/Feature";
+export { default as NavBar } from "./navbar/NavBar";
